fb667ad217daf61915d077cb618e3151bbeeef62
Datorium <hello@datorium.lv>
2020-05-02 17:56:32 +0300

    new feature

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace TestingGithub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            MessageBox.Show("Now the form is green");
+        }
     }
 }
